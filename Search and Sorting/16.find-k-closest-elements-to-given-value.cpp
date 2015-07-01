3a246e975df0c9f9540c145c07fbe98d035ae3f4
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2015-07-02 00:17:26 +0530

    Seach and Sorting uptill 20

--- Search and Sorting/16.find-k-closest-elements-to-given-value.cpp
+++ Search and Sorting/16.find-k-closest-elements-to-given-value.cpp
@@ -33,6 +33,6 @@ void func(int arr[], int n, int k, int x){
 
 int main(){
 	int arr[] = {12, 16, 22, 30, 35, 39, 42, 45, 48, 50, 53, 55, 56};
-	func(arr, sizeof(arr)/sizeof(*arr), 4, 35);
+	func(arr, sizeof(arr)/sizeof(*arr), 5, 35);
 	return 0;
 }
