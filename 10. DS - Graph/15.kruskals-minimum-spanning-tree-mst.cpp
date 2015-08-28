33d6368544dec5eb8a6bc9ac285d344462159bcd
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2015-08-29 00:28:03 +0530

    Graph till 14

---
+++ 10. DS - Graph/15.kruskals-minimum-spanning-tree-mst.cpp
@@ -0,0 +1,66 @@
+// http://www.geeksforgeeks.org/greedy-algorithms-set-2-kruskals-minimum-spanning-tree-mst
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PB push_back
+typedef stack<int> SI;
+typedef queue<int> QI;
+typedef list<int> LI;
+
+struct edge {
+	int src, dest, weight;
+};
+
+struct Graph {
+	int V;
+	int
+};
+
+void function(){
+
+}
+
+int main(){
+	/* Let us create following weighted graph
+             10
+        0--------1
+        |  \     |
+       6|   5\   |15
+        |      \ |
+        2--------3
+            4       */
+    int V = 4;  // Number of vertices in graph
+    int E = 5;  // Number of edges in graph
+    struct Graph* graph = createGraph(V, E);
+
+
+    // add edge 0-1
+    graph->edge[0].src = 0;
+    graph->edge[0].dest = 1;
+    graph->edge[0].weight = 10;
+
+    // add edge 0-2
+    graph->edge[1].src = 0;
+    graph->edge[1].dest = 2;
+    graph->edge[1].weight = 6;
+
+    // add edge 0-3
+    graph->edge[2].src = 0;
+    graph->edge[2].dest = 3;
+    graph->edge[2].weight = 5;
+
+    // add edge 1-3
+    graph->edge[3].src = 1;
+    graph->edge[3].dest = 3;
+    graph->edge[3].weight = 15;
+
+    // add edge 2-3
+    graph->edge[4].src = 2;
+    graph->edge[4].dest = 3;
+    graph->edge[4].weight = 4;
+
+    KruskalMST(graph);
+	return 0;
+}
+
