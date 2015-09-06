5e3c81a6217f4a3630bba5dc579256f1bb19cad3
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-06 15:09:55 +0530

    updated

--- 3. DS - Array/52.maximum-contiguous-circular-sum.cpp
+++ 3. DS - Array/52.maximum-contiguous-circular-sum.cpp
@@ -1,3 +1,5 @@
+//
+
 // http://www.geeksforgeeks.org/maximum-contiguous-circular-sum/
 
 #include<iostream>
@@ -29,10 +31,35 @@ int maxCircularSum(int *a, int n){
 	return maxSum(b, 2*n);
 }
 
+/*Kadane's Method*/
+int kadane(int *a, int n){
+    int max_sum=0, Max=INT_MIN;
+    for(int i=0; i<n; i++){
+        if(max_sum<0){
+            max_sum = 0;
+        }
+        max_sum += a[i];
+        Max = max(Max, max_sum);
+    }
+    return Max;
+}
+
+int maxCircularSum1(int *a, int n){
+    int tmp = kadane(a, n);
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum += a[i];
+        a[i] *= -1;
+    }
+    return max(tmp, sum + kadane(a, n));
+
+}
+
 int main() {
 	int a[] =  {11, 10, -20, 5, -3, -5, 8, -13, 10};
     int n = sizeof(a)/sizeof(a[0]);
     printf("Maximum circular sum is %d\n", maxCircularSum(a, n));
+    printf("Maximum circular sum is %d\n", maxCircularSum1(a, n));
+    getchar();
 	return 0;
 }
-
