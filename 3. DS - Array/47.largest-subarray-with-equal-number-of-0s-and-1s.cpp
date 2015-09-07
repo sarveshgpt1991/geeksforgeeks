eea8966acb9e475fd5de06d1f74101f241e8f959
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-07 17:47:46 +0530

    updated

--- 3. DS - Array/47.largest-subarray-with-equal-number-of-0s-and-1s.cpp
+++ 3. DS - Array/47.largest-subarray-with-equal-number-of-0s-and-1s.cpp
@@ -1,13 +1,19 @@
 // http://www.geeksforgeeks.org/largest-subarray-with-equal-number-of-0s-and-1s/
 
-#include <iostream>
-#include "array.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int *a, int n){
+	for(int i=0; i<n; i++)
+		cout<<a[i]<<" ";
+	cout<<'\n';
+}
 
 void findSubArray(int *a, int n){
 	int leftSum[n];
 	int sum=0;
 	int k=0, l=-1;
-	int max = -1;
+	int Max = -1;
 	for(int i=0; i<n; i++){
 		if(a[i]){
 			sum++;
@@ -22,30 +28,25 @@ void findSubArray(int *a, int n){
 	while(i>=0){
 		if(leftSum[i]==0){
 			l=i;
-			max = l-k+1;
+			Max = l-k+1;
 			break;
 		}
 		i--;
 	}
-	i=0;
-	int j=n-1;
-	while(i <= j && j-i+1 > max){
-		if(leftSum[i]==leftSum[j]){
-			if(j-(i+1)+1 > max){
-				max = j-(i+1)-1;
-				k = (i+1);
-				l = j;
-			}
-			cout<<k<<" to "<<l;
-			return;
+	map<int, int> M;
+	for(int i=0; i<n; i++){
+		if(M.count(leftSum[i]) == 0)
+			M[leftSum[i]] = i;
+		else {
+			Max = max(Max, i-M[leftSum[i]]);
 		}
 	}
-	cout<<k<<" to "<<l;
+	cout<<Max;
 	return;
 }
 
 int main(){
-    int arr[] =  {1, 0, 1, 1, 1, 0, 0};
+    int arr[] =  {1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0};
     int size = sizeof(arr)/sizeof(arr[0]);
     findSubArray(arr, size);
 }
