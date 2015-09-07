a4b48ecabc6a0db37bea6b0c92ada34a52bb8e1d
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-07 18:06:17 +0530

    updated

--- 3. DS - Array/91.rearrange-array-arrj-becomes-arri-j.cpp
+++ 3. DS - Array/91.rearrange-array-arrj-becomes-arri-j.cpp
@@ -1,8 +1,16 @@
-// http://www.geeksforgeeks.org/rearrange-array-arrj-becomes-arri-j/
+// http://www.geeksforgeeks.org/maximum-sum-path-across-two-arrays
+/****/
 
 #include <iostream>
-#include "array.h"
+using namespace std;
 
+void printArray(int *a, int n){
+	for(int i=0; i<n; i++)
+		cout<<a[i]<<" ";
+	cout<<'\n';
+}
+
+//Space O(n)
 void rearrangeNaive(int *a, int n){
 	int help[n];
 	for(int i=0; i<n; i++){
@@ -12,8 +20,33 @@ void rearrangeNaive(int *a, int n){
 	printArray(help, n);
 }
 
+//Sapce O(1)
+int rearrangeUtil(int *a, int i){
+	int idx = a[i]-1;
+	while(a[idx] > 0){
+		int tmp = a[idx];
+		a[idx] = -(i+1);
+		idx = tmp-1;
+		i++;
+	}
+}
+
+int rearrange(int *a, int n){
+	int i;
+    for (i=0; i<n; i++)
+        a[i]++;
+ 
+	for(int i=0; i<n; i++){
+		if(a[i] > 0)
+			rearrangeUtil(a, i);
+	}
+	for(int i=0; i<n; i++){
+		a[i] = -(a[i]+1);
+	}
+}
+
 int main(){
-	int arr[] = {1, 3, 0, 2};
+	int arr[] =  {2, 0, 1, 4, 5, 3};
     int n = sizeof(arr)/sizeof(arr[0]);
  
     printf("Given array is \n");
@@ -21,7 +54,9 @@ int main(){
  
  
     printf("Modified array is \n");
-    rearrangeNaive(arr,  n);
+    //rearrangeNaive(arr,  n);
+    rearrange(arr, n);
+    printArray(arr, n);
+
 	return 0;
 }
-
