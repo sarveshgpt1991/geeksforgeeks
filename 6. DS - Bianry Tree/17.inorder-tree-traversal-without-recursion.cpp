1272216a6e550b1ab1ccdd0d9ba5a7ad967fa8f4
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-21 17:09:46 +0530

    updated

--- 6. DS - Bianry Tree/17.inorder-tree-traversal-without-recursion.cpp
+++ 6. DS - Bianry Tree/17.inorder-tree-traversal-without-recursion.cpp
@@ -27,6 +27,22 @@ void inOrder(node *root){
 	}
 }
 
+void inorder(node *root){
+	if(root == NULL)
+		return;
+	stack<node*> s;
+	node *ptr = root;
+	while(!s.empty() || ptr){
+		while(ptr){
+			s.push_back(ptr);
+			ptr = ptr->left;
+		}
+		ptr = s.back(); s.pop();
+		cout<<ptr->data<<" ";
+		ptr = ptr->right;
+	}
+}
+
 int main(){
 	/* Constructed binary tree is
             1
