00d27e1fd8fac4820b15c2c970026fcdffc420ef
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-21 16:43:02 +0530

    updated

--- 6. DS - Bianry Tree/15.diameter-of-a-binary-tree.cpp
+++ 6. DS - Bianry Tree/15.diameter-of-a-binary-tree.cpp
@@ -3,10 +3,22 @@
 #include <iostream>
 #include "bt.h"
 
+//O(n2)
 int diameter(node *root){
 	if(root == NULL)
 		return 0;
 	return max(max(diameter(root->left), diameter(root->right)), maxDepth(root->left) + 1 + maxDepth(root->right));
+}
+
+// O(n)
+int dia(node *root, int &height){
+	if(root == NULL)
+		return 0;
+	int hr=0, hl=0;
+	int dl = funct(root->left, hl);
+	int dr = funct(root->right, hr);
+	height = 1 + max(hl, hr);
+	return max(max(dl, dr), hl + hr + 1);
 }
 
 int main(){
