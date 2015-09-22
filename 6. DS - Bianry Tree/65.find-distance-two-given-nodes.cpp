c634425581c06aff2e0f5dea781fbb8cad21c0fc
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-22 20:56:08 +0530

    updated

--- 6. DS - Bianry Tree/65.find-distance-two-given-nodes.cpp
+++ 6. DS - Bianry Tree/65.find-distance-two-given-nodes.cpp
@@ -29,6 +29,69 @@ int findDistance(node *root, int k1, int k2){
 	return path1.size()-i + path2.size()-i;
 }
 
+/* LCA method */
+/*LCA*/
+bool find(node *root, int n){
+	if(root == NULL)
+		return false;
+	if(root->data == n)
+		return true;
+	if(find(root->left, n) || find(root->right, n)){
+		return 1;
+	}
+	return 0;
+}
+
+node *LCA(node *root, int n1, int n2){
+	if(root == NULL)
+		return NULL;
+	if(root->data == n1 || root->data == n2){
+		return root;
+	}
+	node *l = LCA(root->left, n1, n2);
+	node *r = LCA(root->right, n1, n2);
+	if(l && r){
+		return root;
+	}
+	return ((l != NULL) ? l : r);
+}
+
+node *findLCA(node *root, int n1, int n2){
+	node *res = LCA(root, n1, n2);
+	if(res->data != n1 && res->data != n2)
+		return res;
+	if(res->data == n1){
+		return (find(res, n2) ? res : NULL);
+	}
+	return (find(res, n1) ? res : NULL);
+}
+/*End*/
+
+void findLevel(node *root, int n1, int n2, int n3, int &d1, int &d2, int &d3, int level){
+	if(root == NULL)
+		return;
+	if(root->data == n1){
+		d1 = level;
+	}
+	else if(root->data == n2){
+		d2 = level;
+	}
+	else if(root->data == n3){
+		d3 = level;
+	}
+	findLevel(root->left, n1, n2, n3, d1, d2, d3, level+1);
+	findLevel(root->right, n1, n2, n3, d1, d2, d3, level+1);
+}
+
+int findDistance(node *root, int n1, int n2){
+    node *lca = findLCA(root, n1, n2);
+    int d1, d2, l;
+	findLevel(root, n1, n2, lca->data, d1, d2, l, 0);
+    if (d1 != -1 && d2 != -1)
+           return d1+d2-2*l;
+    return -1;
+}
+
 int main(){
 	node * root = newNode(1);
     root->left = newNode(2);
