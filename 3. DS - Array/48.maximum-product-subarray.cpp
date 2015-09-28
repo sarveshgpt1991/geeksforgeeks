3a48285ab81ecf42e7ebd411d8ab6e6dcce8edd8
Sarvesh Gupta <sarvesh.onlyme@gmail.com>
2015-09-29 00:29:59 +0530

    updated

--- 3. DS - Array/48.maximum-product-subarray.cpp
+++ 3. DS - Array/48.maximum-product-subarray.cpp
@@ -26,6 +26,29 @@ int maxSubarrayProduct(int *arr, int n){
     return max_so_far;
 }
 
+// All case covered
+int maxProduct(const vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n = A.size();
+    int max_overall = INT_MIN;
+    int max_here = A[0];
+    int min_here = A[0];
+    if(n == 1){
+        return A[0];
+    }
+    for(int i=1; i<n; i++){
+        int maxProd = A[i]*max_here;
+        int minProd = A[i]*min_here;
+        max_here = max(A[i], max(maxProd, minProd));
+        min_here = min(A[i], min(maxProd, minProd));
+        max_overall = max(max_here, max_overall);
+    }
+    return max_overall;
+}
+
 int main(){
 	int arr[] = {1, -2, -3, 0, 7, -8, -2};
     int n = sizeof(arr)/sizeof(arr[0]);
